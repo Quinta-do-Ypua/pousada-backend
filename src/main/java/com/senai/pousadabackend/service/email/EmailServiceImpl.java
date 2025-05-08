0707c980b3d16c60927a49a5cf03d57ed593e7a0
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2025-05-07 21:32:10 -0300

    Envio de email ao efetuar e cancelar reserva.

--- src/main/java/com/senai/pousadabackend/service/email/EmailServiceImpl.java
+++ src/main/java/com/senai/pousadabackend/service/email/EmailServiceImpl.java
@@ -9,14 +9,12 @@ import com.sendgrid.helpers.mail.objects.Email;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
 @Slf4j
-@ConfigurationProperties("sendgrid")
 public class EmailServiceImpl implements EmailService {
 
     @Value("${sendgrid.key}")
@@ -35,16 +33,17 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void enviar(String assunto, String message, Cliente cliente) {
         Email to = new Email(cliente.getEmail());
-
         Content content = new Content("text/plain", message);
+
         Mail mail = new Mail(from, assunto, to, content);
         SendGrid sg = new SendGrid(apiKey);
-        Request request = new Request();
 
+        Request request = new Request();
         try {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
+
             Response response = sg.api(request);
             log.info("Status: {}", response.getStatusCode());
             log.info("Body: {}", response.getBody());
