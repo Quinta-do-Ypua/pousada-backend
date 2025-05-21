e4a268114648fbbb2d795205018363221116f23b
kauan-cancelier <kauanmellocancelier2005@gmail.com>
2025-05-20 21:00:12 -0300

    Validação de unique em quarto.

--- src/main/java/com/senai/pousadabackend/repository/QuartoRepository.java
+++ src/main/java/com/senai/pousadabackend/repository/QuartoRepository.java
@@ -3,4 +3,6 @@ package com.senai.pousadabackend.repository;
 import com.senai.pousadabackend.entity.Quarto;
 
 public interface QuartoRepository extends BaseRepository<Quarto, Long> {
+
+    boolean existsByNumero(String numero);
 }
