029fa308f30963726ae781385e04d894311b179f
MuriloVieiraCruz <murilo12super@gmail.com>
2025-06-05 21:17:04 -0300

    Ajustado mensagem de erro ao excluir uma imagem inexistente/ já excluída

--- src/main/java/com/senai/pousadabackend/domain/Imagem/service/ImagemServiceImpl.java
+++ src/main/java/com/senai/pousadabackend/domain/Imagem/service/ImagemServiceImpl.java
@@ -5,8 +5,6 @@ import com.senai.pousadabackend.domain.Imagem.ImagemRepository;
 import com.senai.pousadabackend.domain.quarto.Quarto;
 import com.senai.pousadabackend.domain.quarto.service.QuartoService;
 import com.senai.pousadabackend.exceptions.BusinessException;
-import com.senai.pousadabackend.integration.DeleteQuarto;
-import com.senai.pousadabackend.integration.UploadQuarto;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,20 +15,15 @@ import java.util.List;
 public class ImagemServiceImpl implements ImagemService {
 
     private final QuartoService quartoService;
-    private final UploadQuarto uploadQuarto;
-    private final DeleteQuarto deleteQuarto;
+
     private final ImagemRepository repository;
 
     public ImagemServiceImpl(
             @Qualifier("quartoServiceImpl")
             QuartoService quartoService,
-            UploadQuarto uploadQuarto,
-            DeleteQuarto deleteQuarto,
             ImagemRepository repository) {
         this.quartoService = quartoService;
-        this.uploadQuarto = uploadQuarto;
         this.repository = repository;
-        this.deleteQuarto = deleteQuarto;
     }
 
     private static final long TAMANHO_MAXIMO_ARQUIVO = 5 * 1024 * 1024;
@@ -64,8 +57,6 @@ public class ImagemServiceImpl implements ImagemService {
     @Override
     public void deletar(ImagemQuarto imagemQuarto) {
         this.validar((imagemQuarto));
-
-        deleteQuarto.deletarImagem(imagemQuarto.getFileId());
         repository.deleteById(imagemQuarto.getId());
     }
 
--- src/main/java/com/senai/pousadabackend/domain/Imagem/service/ImagemServiceProxy.java
+++ src/main/java/com/senai/pousadabackend/domain/Imagem/service/ImagemServiceProxy.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.senai.pousadabackend.domain.Imagem.ImagemQuarto;
+import com.senai.pousadabackend.exceptions.BusinessException;
+import com.senai.pousadabackend.integration.DeleteQuarto;
 import com.senai.pousadabackend.integration.UploadQuarto;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,14 +20,17 @@ public class ImagemServiceProxy implements ImagemService {
 
     private final ImagemService service;
     private final UploadQuarto uploadQuarto;
+    private final DeleteQuarto deleteQuarto;
 
     public ImagemServiceProxy(
             @Qualifier("imagemServiceImpl")
             ImagemService service,
-            UploadQuarto uploadQuarto
+            UploadQuarto uploadQuarto,
+            DeleteQuarto deleteQuarto
     ) {
         this.service = service;
         this.uploadQuarto = uploadQuarto;
+        this.deleteQuarto = deleteQuarto;
     }
 
     @Override
@@ -69,5 +75,15 @@ public class ImagemServiceProxy implements ImagemService {
     @Override
     public void deletar(ImagemQuarto imagemQuarto) {
         service.deletar(imagemQuarto);
+
+        try {
+            deleteQuarto.deletarImagem(imagemQuarto.getFileId());
+        } catch (FeignException e) {
+            if (e.status() == 404) {
+                throw new BusinessException("Imagem inexistente ou já excluída: " + imagemQuarto.getFileId());
+            }
+
+            throw new BusinessException("Erro ao excluir a imagem. Motivo: " + e.getMessage());
+        }
     }
 }
