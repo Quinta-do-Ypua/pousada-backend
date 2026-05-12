a95061506de4c9d06867963426f1079c4cda30c2
murilovieiracruz <murilo12super@gmail.com>
2026-05-11 23:21:13 -0300

    retirado classes não utilizadas

--- src/main/java/com/senai/pousadabackend/exceptions/SenhaInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package com.senai.pousadabackend.exceptions;
-
-public class SenhaInvalidaException extends RuntimeException {
-    public SenhaInvalidaException() {
-        super("Senha incorreta");
-    }
-}
